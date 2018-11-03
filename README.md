15591be0a36e15ee4a59b3dd336a318752dcf896
bcsuser <bellevuecodeschool@gmail.com>
2018-11-03 10:42:02 -0700

    Update README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to Spy Project
+#### This is a homework for GeekDojo Web Programming class
+This folder contains `app.py` and `templates` folder.
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/gadgets')
+def gadgets():
+    gadgets = ['Spy Cam', 'Silencer', 'Pocket Knife', 'Bulletproof Vest', 'Desert Eagle', 'Pen']
+    return render_template('gadgets.html', title='Gadgets', subtitle='Essential gadgets for every mission!', gadgets=gadgets)
+
+@app.route('/missions')
+def missions():
+    missions = ['Politican escort In Russia', 'North Korean Intel Corps', 'Malaysian Base Infiltration', 'Russian Nuclear Bomb Code Sabatoge']
+    return render_template('missions.html', title='Missions', subtitle='Pending missions' , missions=missions)
+
+@app.route('/')
+def home():    
+    return render_template('home.html', title='Welcome to Pet Rescue')
+
+if __name__ == '__main__':
+    app.run(debug=True)
